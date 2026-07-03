f422bfcffa661e95001e6c0aca648f9fda7c43b3
lingeswaran05 <160377031+lingeswaran05@users.noreply.github.com>
2026-07-03 11:50:15 +0530

    [1] [Time Beats: 48.25%] [Memory Beats: 92.91%] - LeetPush

---
+++ 1-Two-Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] res=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int comp=target-nums[i];
+            if(map.containsKey(comp)){
+                res[0] = map.get(comp);
+                res[1]=i;
+            }
+            else
+                map.put(nums[i],i);
+        }
+        return res;
+    }
+}
